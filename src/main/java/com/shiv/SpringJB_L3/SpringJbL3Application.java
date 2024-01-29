596ff88cc3d8ccd91fce841afa8bc2072c95969e
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-01-29 23:03:01 +0530

    Initial Commit

---
+++ src/main/java/com/shiv/SpringJB_L3/SpringJbL3Application.java
@@ -0,0 +1,13 @@
+package com.shiv.SpringJB_L3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJbL3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJbL3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiv/SpringJB_L3/SpringJbL3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiv.SpringJB_L3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJbL3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
