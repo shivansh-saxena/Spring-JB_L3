15618476c2737f80dc38308f29710fe55bf8ec94
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-01-29 23:09:55 +0530

    fetching value from properties

---
+++ src/main/java/com/shiv/contoller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.shiv.contoller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Value("{my.greeting}")
+    private String valueFromProperties;
+    @GetMapping("/")
+    public String greeting()
+    {
+        return valueFromProperties;
+    }
+}
