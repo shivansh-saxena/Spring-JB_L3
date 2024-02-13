6f7a8c18812131e114090812e112bcd6f32a4251
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-02-13 22:35:05 +0530

    Features of @Value annotation -we can have static value in it with just double quotes "" -we can store list in properties and access them just by reading values in a List object -we can store key value pairs as well in properties, only change is we read it using a Map and Syntax inside @Value changes eg. "#{${my.keyValue}}" addition over here is #

--- src/main/java/com/shiv/contoller/GreetingController.java
+++ src/main/java/com/shiv/contoller/GreetingController.java
@@ -4,14 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
     @Value("${my.greeting: default value for properties}")
     private String valueFromProperties;
+    @Value("some static string")
+    private String staticString;
+    @Value("${my.list}")
+    private List<String> myList;
+    @Value("#{${my.keyValue}}")
+    private Map<String,String> keyValue;
     @GetMapping("/")
     public String greeting()
     {
-        return valueFromProperties;
+        return valueFromProperties+"\n  "+myList+staticString+"\n  "+keyValue;
     }
 }
