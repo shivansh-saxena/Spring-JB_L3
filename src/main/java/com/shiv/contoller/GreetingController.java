2042ea0d38346f26415a89210d40fe5b4f5d32ea
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-02-14 23:26:50 +0530

    introducing Environment for fetching different values, but it is not recommended to use it and make code environment specific

--- src/main/java/com/shiv/contoller/GreetingController.java
+++ src/main/java/com/shiv/contoller/GreetingController.java
@@ -3,6 +3,7 @@ package com.shiv.contoller;
 import com.shiv.settings.MySettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +23,19 @@ public class GreetingController {
     private Map<String,String> keyValue;
     @Autowired
     private MySettings mySettings;
+    @Autowired
+    private Environment environment;
     @GetMapping("/")
     public String greeting()
     {
         return valueFromProperties+"\n  "+myList+staticString+"\n  "+keyValue+"\n" +
                 "  Values from mySettings using ConfigurationProperties; "+mySettings;
     }
+
+    @GetMapping("/env")
+    public String envDetails()
+    {
+        return environment.toString();
+    }
+
 }
